e4225c57907be577dd54b359493f6953983eacf1
Angelorigo <angel.rodrigo.servin@gmail.com>
2025-06-21 21:31:20 -0600

    Segundo avance

--- fibonacci_dinamico.cpp
+++ fibonacci_dinamico.cpp
@@ -4,3 +4,32 @@
 //    PROGRAMACIÓN DINÁMICA
 //    ANÁLISIS DE ALGORITMOS
 //================================
+
+#include <iostream>
+using namespace std;
+
+const int MAX = 100;
+int Tabla[MAX]; 
+
+int FibonacciDinamico(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    if (Tabla[n] == 0) 
+        Tabla[n] = FibonacciDinamico(n - 1) + FibonacciDinamico(n - 2);
+    return Tabla[n];
+}
+
+int main() {
+    int n;
+    cout << "Introduce el valor de n: ";
+    cin >> n;
+
+    for (int i = 0; i <= n; ++i)
+        Tabla[i] = 0;
+
+    int resultado = FibonacciDinamico(n);
+    cout << "El Fibonacci dinamico de " << n << " es: " << resultado << endl;
+
+    return 0;
+}
+
